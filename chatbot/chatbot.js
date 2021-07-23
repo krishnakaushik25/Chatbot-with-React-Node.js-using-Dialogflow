cd34ea14f80b51ff77cf428bca4b0d8b0ef81f63
krishna11 <krishna11@github.com>
2021-07-23 18:59:18 +0530

    connect server and dialogflow

---
+++ chatbot/chatbot.js
@@ -0,0 +1,69 @@
+'use strict';
+const dialogflow = require('dialogflow');
+const config = require('../config/keys');
+const structjson = require('./structjson.js');
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
+
+const credentials = {
+    client_email: config.googleClientEmail,
+    private_key:config.googlePrivateKey,
+};
+
+const sessionClient = new dialogflow.SessionsClient({projectId, credentials});
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+
+module.exports = {
+    textQuery: async function(text, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: text,
+                    languageCode: languageCode,
+                },
+            },
+            queryParams: {
+                payload: {
+                    data: parameters
+                }
+            }
+        };
+
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+
+
+
+    },
+    eventQuery: async function(event, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    name: event,
+                    parameters: structjson.jsonToStructProto(parameters), //Dialogflow's v2 API uses gRPC. You'll need a jsonToStructProto method to convert your JavaScript object to a proto struct.
+                    languageCode: languageCode,
+                },
+            }
+        };
+
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+
+    },
+
+
+
+    handleAction: function(responses){
+        return responses;
+    },
+
+
+}
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-app.get('/', (req, res) => {
-    res.send({'hi': 'there'})
- });
- const PORT = process.env.PORT || 5000;
- app.listen(PORT);
+
+app.use(express.json()); 
+
+require('./routes/dialogFlowRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
